dc414f9201b6ded4c50bf1b6a0f1fa61829c50e0
shakil51298 <zikaiwen02@gmail.com>
2021-02-23 02:38:49 +0800

    similer in look,diffrent in data, dynamic css

--- src/App.js
+++ src/App.js
@@ -2,24 +2,43 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+const personDetails = {
+  fName: 'Shakil',
+  LName: 'Khan'
+}
+var myStyle = {
+  color:'red',
+  padding: '10px',
+  border: '1px solid red',
+  backgroundColor: 'yellow',
+  borderRadius: '15px'
+}
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>i am a react person</p>
+        <MyComponent Name="Shakil Khan" Fname="Khan"></MyComponent>  {/* call component */}
+        <MyComponent Name="Shakib Khan" Fname="Khan"></MyComponent>
+        <MyComponent Name="Azad Khan" Fname="Molla"></MyComponent>
+        <MyComponent Name="Azad Khan" Fname="Khan"></MyComponent>
       </header>
     </div>
   );
 }
+
+//component creat
+function MyComponent(props) { //should write first latter uppercase
+  var Shakil= {
+    color: 'salmon'
+  }
+  return (
+    <div style={Shakil}>
+      <h1> Name: {props.Name} </h1>
+      <h3> FName: {props.Fname}</h3>
+    </div>
+  )
+}
+
 
 export default App;
