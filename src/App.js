6d501e3ff7a5955645017555d6c31018d678e4b8
shakil51298 <zikaiwen02@gmail.com>
2021-02-23 21:48:07 +0800

    Counter, State,Dtnamic with map

--- src/App.js
+++ src/App.js
@@ -1,44 +1,77 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-const personDetails = {
-  fName: 'Shakil',
-  LName: 'Khan'
-}
-var myStyle = {
-  color:'red',
-  padding: '10px',
-  border: '1px solid red',
-  backgroundColor: 'yellow',
-  borderRadius: '15px'
-}
-
+const products = [
+  {name:'Laptop', price: '$500'},
+  {name: 'Mobile', price:' $ 480'},
+  {name: 'Charger', price:'$40'},
+  {name: 'Clokc', price: '$70'}
+]
+const mYarrya = ['shakil','shakil','amar baba','rozina']
+//const arrays = ['shakil','azad dsd','koly','rajib']; // from array
   return (
     <div className="App">
       <header className="App-header">
-        <p>i am a react person</p>
-        <MyComponent Name="Shakil Khan" Fname="Khan"></MyComponent>  {/* call component */}
-        <MyComponent Name="Shakib Khan" Fname="Khan"></MyComponent>
+      <Count></Count>
+
+        {/* <MyComponent Name={arrays[0]} Fname="Khan"></MyComponent>  {/* call component */}
+        {/* <MyComponent Name={arrays[1]} Fname="Khan"></MyComponent>
         <MyComponent Name="Azad Khan" Fname="Molla"></MyComponent>
-        <MyComponent Name="Azad Khan" Fname="Khan"></MyComponent>
+        <MyComponent Name="Azad Khan" Fname="Khan"></MyComponent> } */}
+      <ul>
+        {
+          mYarrya.map(ss => <li>{ss}</li>)
+        }        
+        
+        {
+          products.map(proName => <li>{proName.name}</li>)
+        }
+        
+      </ul>
+
+      <Products product={Products[0]}></Products>
       </header>
     </div>
   );
 }
+function Products(props) {
+console.log(props.name);
 
-//component creat
-function MyComponent(props) { //should write first latter uppercase
-  var Shakil= {
-    color: 'salmon'
-  }
-  return (
-    <div style={Shakil}>
-      <h1> Name: {props.Name} </h1>
-      <h3> FName: {props.Fname}</h3>
+  return(
+    <div >
+      <h5></h5>
+      <h3></h3>
+      <button>Buy Now</button>
     </div>
   )
 }
+//  Count Funtion
+function Count() {
+  const [data,setData] = useState(0)
+  const handleCount = () => setData(data + 1)
+  const countDecrease = () => setData(data - 1)
+  return(
+    <div>
+      <h1>Count: {data}</h1>
+      <button onClick={handleCount}>Increase + </button>
+      <button onClick={countDecrease}>Decrese - </button>
+    </div>
+  )
+}
+// //component creat
+// function MyComponent(props) { //should write first latter uppercase
+//   var Shakil= {
+//     color: 'salmon'
+//   }
+//   return (
+//     <div style={Shakil}>
+//       <h1> Name: {props.Name} </h1>
+//       <h3> FName: {props.Fname}</h3>
+//     </div>
+//   )
+// }
 
 
 export default App;
